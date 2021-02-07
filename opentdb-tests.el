1bcfb93c6fbc3af85e08b9c0a30bd196405804a7
Karl Johansson <karljoh85@gmail.com>
2021-02-07 21:08:39 +0100

    add a way to use test packages

--- opentdb-tests.el
+++ opentdb-tests.el
@@ -1,3 +1,40 @@
+(require 'opentdb)
+(require 'request)
+
+;; Not in use yet
+(defvar mock-response "
+{
+  \"response_code\": 0,
+  \"results\": [
+    {
+      \"category\": \"RW50ZXJ0YWlubWVudDogSmFwYW5lc2UgQW5pbWUgJiBNYW5nYQ==\",
+      \"type\": \"bXVsdGlwbGU=\",
+      \"difficulty\": \"ZWFzeQ==\",
+      \"question\": \"SW4gdGhlIDl0aCBQb2tlbW9uIG1vdmllLCB3aG8gaXMgdGhlIFByaW5jZSBvZiB0aGUgU2VhPw==\",
+      \"correct_answer\": \"TWFuYXBoeQ==\",
+      \"incorrect_answers\": [
+        \"QXNo\",
+        \"TWF5\",
+        \"UGhhbnRvbQ==\"
+      ]
+    },
+    {
+      \"category\": \"RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=\",
+      \"type\": \"bXVsdGlwbGU=\",
+      \"difficulty\": \"bWVkaXVt\",
+      \"question\": \"V2hhdCB0eXBlIG9mIGdlbnJlIGlzIHRoZSBjb250cm92ZXJzaWFsIDIwMTUgZ2FtZSAiSGF0cmVkIj8=\",
+      \"correct_answer\": \"U2hvb3QgJ0VtIFVw\",
+      \"incorrect_answers\": [
+        \"UG9pbnQgJiBDbGljaw==\",
+        \"TU1PUlBH\",
+        \"U2ltdWxhdGlvbg==\"
+      ]
+    }
+  ]
+}
+")
+
+;; TODO, mock requests and assert some stuffz
 (ert-deftest dummy-test ()
-  "Just a dummy test to get started"
-  (should (= 4 4)))
+   (opentdb-fetch-questions)
+   (should (= 4 4)))
