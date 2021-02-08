a4ac02bcf6917e05ca3c6961313d86272d3d4315
Karl Johansson <karljoh85@gmail.com>
2021-02-08 19:40:47 +0100

    Add som proper testing

--- opentdb-tests.el
+++ opentdb-tests.el
@@ -1,40 +1,27 @@
 (require 'opentdb)
 (require 'request)
 
-;; ;; Not in use yet
-;; (defvar mock-response "
-;; {
-;;   \"response_code\": 0,
-;;   \"results\": [
-;;     {
-;;       \"category\": \"RW50ZXJ0YWlubWVudDogSmFwYW5lc2UgQW5pbWUgJiBNYW5nYQ==\",
-;;       \"type\": \"bXVsdGlwbGU=\",
-;;       \"difficulty\": \"ZWFzeQ==\",
-;;       \"question\": \"SW4gdGhlIDl0aCBQb2tlbW9uIG1vdmllLCB3aG8gaXMgdGhlIFByaW5jZSBvZiB0aGUgU2VhPw==\",
-;;       \"correct_answer\": \"TWFuYXBoeQ==\",
-;;       \"incorrect_answers\": [
-;;         \"QXNo\",
-;;         \"TWF5\",
-;;         \"UGhhbnRvbQ==\"
-;;       ]
-;;     },
-;;     {
-;;       \"category\": \"RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=\",
-;;       \"type\": \"bXVsdGlwbGU=\",
-;;       \"difficulty\": \"bWVkaXVt\",
-;;       \"question\": \"V2hhdCB0eXBlIG9mIGdlbnJlIGlzIHRoZSBjb250cm92ZXJzaWFsIDIwMTUgZ2FtZSAiSGF0cmVkIj8=\",
-;;       \"correct_answer\": \"U2hvb3QgJ0VtIFVw\",
-;;       \"incorrect_answers\": [
-;;         \"UG9pbnQgJiBDbGljaw==\",
-;;         \"TU1PUlBH\",
-;;         \"U2ltdWxhdGlvbg==\"
-;;       ]
-;;     }
-;;   ]
-;; }
-;; ")
+(ert-deftest test-fetch-questions-returns-found-questions ()
+  ;; Mockdata returns 2 questions
+  (should (= 2 (length (opentdb-fetch-questions)))))
 
-;; TODO, mock requests and assert some stuffz
-(ert-deftest dummy-test ()
-   (opentdb-fetch-questions))
-   ;; (should (= 4 4)))
+
+(ert-deftest test-fetch-questions-are-set-correctly ()
+  (let* ((question (nth 0 (opentdb-fetch-questions)))
+	 (category
+	  (base64-encode-string (opentdb-question-category question)))
+	 (type
+	  (base64-encode-string (opentdb-question-type question)))
+	 (difficulty
+	  (base64-encode-string (opentdb-question-difficulty question)))
+	 (quiz
+	  (base64-encode-string (opentdb-question-question question)))
+	 (correct-answer
+	  (base64-encode-string (opentdb-question-correct-answer question))))
+
+    ;; Check mock-request.el for test data
+    (should (equal "RW50ZXJ0YWlubWVudDogSmFwYW5lc2UgQW5pbWUgJiBNYW5nYQ==" category))
+    (should (equal "bXVsdGlwbGU=" type))
+    (should (equal "ZWFzeQ==" difficulty))
+    (should (equal "SW4gdGhlIDl0aCBQb2tlbW9uIG1vdmllLCB3aG8gaXMgdGhlIFByaW5jZSBvZiB0aGUgU2VhPw==" quiz))
+    (should (equal "TWFuYXBoeQ==" correct-answer))))
